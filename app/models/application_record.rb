1ed006ee1a49a1625e095e7a3bbcf22fe4000390
Serje <Serje.havandjian@gmail.com>
2022-06-26 18:38:30 -0700

    commit tree 96787933871a369abd33537bab4d490cf325ce8d

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+
+
+    # A typical model file will contain code such as the following:
+    # 1) Custom Scopes, 2) Model Instance Methods, 3) Default settings for databse columns, 4) Validations, 5) Model-to-model relationships, 5) Callbacks, 6) Custom Algorithms
+
 end
